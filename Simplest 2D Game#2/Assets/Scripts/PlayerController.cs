715c0315f6efe051db5ac414bf038255f77a1db4
codenameone(Home) <playstation3.infamous@gmail.com>
2017-05-08 04:50:29 +0530

    Added Basic Movement, Jumping, Avoiding Double Jumping. Basic Ground Sprite Added with Collider. Collider Added to Player and Animating the player.

---
+++ Simplest 2D Game#2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+
+    public float moveForce = 400f;
+    public float maxSpeed = 7f;
+    public float jumpForce = 3000f;
+    public Transform groundCheck;
+
+    public bool grounded = false;
+    public Animator anim;
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb.velocity.x < maxSpeed)
+            rb.AddForce(Vector2.right * h * moveForce);
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+        if (jump)
+        {
+            anim.Play("Jump");
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
