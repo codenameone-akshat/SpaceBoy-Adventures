772d0e5bc311034fcf60f7a7b079977e74db62d3
codenameone(Home) <playstation3.infamous@gmail.com>
2017-05-09 02:27:14 +0530

    Added Prefabs to create environment. Failed to create a destroyer.

---
+++ Simplest 2D Game#2/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    public Transform player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x - 10, 0f, 0f);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject.Destroy(collision.gameObject);
+    }
+}   
--- Simplest 2D Game#2/Assets/Scripts/EnemyMovement.cs
+++ Simplest 2D Game#2/Assets/Scripts/EnemyMovement.cs
@@ -37,7 +37,7 @@ public class EnemyMovement : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Player"))
         {
             Flip();
         }
