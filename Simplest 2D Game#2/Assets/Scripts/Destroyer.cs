d8bb671f84165c179c1500b274b50afceb69ea2f
codenameone(Home) <playstation3.infamous@gmail.com>
2017-05-09 02:53:03 +0530

    Comments aboout how to destroy the fixed joints added.

--- Simplest 2D Game#2/Assets/Scripts/Destroyer.cs
+++ Simplest 2D Game#2/Assets/Scripts/Destroyer.cs
@@ -8,10 +8,11 @@ public class Destroyer : MonoBehaviour {
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x - 10, 0f, 0f);
+        transform.position = new Vector3(player.position.x - 40, 0f, 0f);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject.Destroy(collision.gameObject);
+        GameObject.Destroy(collision.gameObject); // this destroys a rigidbody only
+        //refer internet to learn how to destroy fixed joints
     }
 }   
