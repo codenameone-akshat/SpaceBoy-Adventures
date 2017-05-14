537edc0beecae3c001b6c932e11263c90c42bb57
codenameone(Home) <playstation3.infamous@gmail.com>
2017-05-15 02:49:02 +0530

    Created Another Level. Added Level Transition. Added new enemies. Added new animation for the new enemies.

---
+++ Simplest 2D Game#2/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            SceneManager.LoadScene(0);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
